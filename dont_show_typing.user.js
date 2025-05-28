437441bbf7c7c906ac5f015df756a0c4b50db039
NeverDecaf <jtr9289@gmail.com>
2025-05-28 06:16:03 -0400

    add typing blocker

---
+++ dont_show_typing.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Hide Typing Status on Discord
+// @version      1.0
+// @description  Prevents other users from seeing when you are typing.
+// @author       NeverDecaf
+// @match        https://discord.com/*
+// @require      https://neverdecaf.github.io/discord-userscripts/webpackmodules.js
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+(function () {
+	"use strict";
+
+	runAfterWMLoaded(main);
+
+	function main() {
+		const UsedModules = {
+			dispatcher: window.Filters.byKeys([
+				"dispatch",
+				"subscribe",
+				"register",
+			]),
+		};
+		waitForAllModules(UsedModules).then(() => {
+			const originalDispatch = UsedModules.dispatcher.dispatch;
+			UsedModules.dispatcher.dispatch = function (action, ...rest) {
+				if (action?.type === "TYPING_START_LOCAL") return;
+				return originalDispatch.apply(this, [action, ...rest]);
+			};
+		});
+	}
+})();
