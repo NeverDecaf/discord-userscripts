4fc83d76704b22d60e6fbc2037a9cd805356580e
NeverDecaf <jtr9289@gmail.com>
2025-06-19 13:09:45 -0400

    modify prototype instead

--- filename_randomizer.user.js
+++ filename_randomizer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Discord Filename Randomizer
-// @version      0.1
+// @version      0.2
 // @description  "Randomize" filenames of uploaded files on discord by replacing them with the current timestamp
 // @author       NeverDecaf
 // @match        https://discord.com/*
@@ -12,7 +12,7 @@
     "use strict";
     WMInit().then(() => {
         const Modules = {
-            fileUploads: Filters.byKeys(["uploadFiles"]),
+            fileUploads: Filters.byStrings(["uploadFiles"]),
         };
         function randomizeFilename(originalFilename) {
             let ext = originalFilename.split(".").pop();
@@ -22,9 +22,10 @@
             return `${Date.now().toString()}${ext}`;
         }
         waitForAllModules(Modules).then(() => {
-            const originalUploadFiles = Modules.fileUploads.uploadFiles;
-            Modules.fileUploads.uploadFiles = function () {
-                for (const file of arguments[0].uploads) {
+            const originalUploadFiles =
+                Modules.fileUploads.prototype.uploadFiles;
+            Modules.fileUploads.prototype.uploadFiles = function () {
+                for (const file of arguments[0]) {
                     file.filename = randomizeFilename(file.filename);
                 }
                 return originalUploadFiles.apply(this, arguments);
