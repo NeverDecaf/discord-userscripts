39392cfe18dfa06aa566ac78d93c286c6c224350
NeverDecaf <jtr9289@gmail.com>
2026-02-01 18:54:48 -0500

    fix

--- dont_show_typing.user.js
+++ dont_show_typing.user.js
@@ -1,19 +1,17 @@
 // ==UserScript==
 // @name         Hide Typing Status on Discord
-// @version      1.2
+// @version      1.2.1
 // @description  Prevents other users from seeing when you are typing.
 // @author       NeverDecaf
 // @match        https://discord.com/*
-// @require      https://neverdecaf.github.io/discord-userscripts/webpackmodules.js?v=1
+// @require      https://neverdecaf.github.io/discord-userscripts/webpackmodules.js?v=2
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
 (function () {
 	"use strict";
 	WMInit().then(() => {
-		const Modules = {
-			Dispatcher: Filters.byKeys(["dispatch", "subscribe", "register"]),
-		};
+		const Modules = {};
 		waitForAllModules(Modules).then(() => {
 			const originalDispatch = Modules.Dispatcher.dispatch;
 			Modules.Dispatcher.dispatch = function (action, ...rest) {
